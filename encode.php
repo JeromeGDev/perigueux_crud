04ea6ca7aede01b86fe29c800789c88585294ded
JeromeGDev <85901454+JeromeGDev@users.noreply.github.com>
2023-01-12 07:48:43 +0100

    suppression fichier inutiles

--- encode.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// $passwordCrypt = sha1(sha1('123',$userPsdForm .'salage'));// (ne pas aller plus loin) double cryptage sha1 + '123' et 'salage' = chaines de caractere a choisir au hasard, mais défintif qui augmente le cryptage
-// $passwordCrypt = sha1(sha1('123',$userPsd .'salage'));// (ne pas aller plus loin) double cryptage sha1 + '123' et 'salage' = chaines de caractere a choisir au hasard, mais défintif qui augmente le cryptage
-
-
-function encode($a){
-    return sha1(sha1('123',$a.'salage'));// (ne pas aller plus loin) double cryptage sha1 + '123' et 'salage' = chaines de caractere a choisir au hasard, mais défintif qui augmente le cryptage
-}
